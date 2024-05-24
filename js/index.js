ba2c68100b1f96db74f6eebadef995d66e7757c7
Diego Nathanael <115087435+DNathanael19@users.noreply.github.com>
2024-05-23 22:48:58 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('.lista li a');
     const menu = document.querySelector('.menu');
+    const sections = document.querySelectorAll('.section');
+
+    function animateSection(targetSection) {
+        sections.forEach(section => {
+            section.classList.remove('fade-in');
+        });
+
+        targetSection.classList.add('fade-in');
+    }
 
     links.forEach(link => {
         link.addEventListener('click', function(e) {
@@ -9,18 +18,10 @@ document.addEventListener('DOMContentLoaded', function() {
             const targetId = this.getAttribute('href');
             const targetSection = document.querySelector(targetId);
 
-            // Remove a classe de animação de todas as seções
-            document.querySelectorAll('.section').forEach(section => {
-                section.classList.remove('fade-in');
-            });
+            animateSection(targetSection);
 
-            // Adiciona a classe de animação à seção alvo imediatamente após clicar no link
-            targetSection.classList.add('fade-in');
-
-            // Calcula a posição da seção alvo, considerando o deslocamento do menu fixo
             const targetOffset = targetSection.getBoundingClientRect().top + window.scrollY - menu.offsetHeight;
 
-            // Faz a rolagem suave até a seção alvo
             window.scrollTo({
                 top: targetOffset,
                 behavior: 'smooth'
@@ -28,16 +29,8 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // Adiciona animação para a primeira seção quando a página é carregada
     const firstSection = document.querySelector('.section');
-    firstSection.classList.add('fade-in');
-
-    // Adiciona animação para todas as seções quando clicar em qualquer link do menu
-    links.forEach(link => {
-        link.addEventListener('click', function() {
-            document.querySelectorAll('.section').forEach(section => {
-                section.classList.add('fade-in');
-            });
-        });
-    });
+    if (firstSection) {
+        firstSection.classList.add('fade-in');
+    }
 });
